3f05665616efdcb1ee83b5899177d13ba0a997f5
jeffKim <kwangho5538@gmail.com>
2024-05-04 10:53:27 +0900

    Add Images

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:motivational_wallpapers/home_screen.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     ),
   );
