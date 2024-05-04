846880727a2fc8f5b085dc33d3f3e5f7c44bb27c
jeffKim <kwangho5538@gmail.com>
2024-05-04 11:53:15 +0900

    Add Controller

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,12 +1,53 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+  PageController controller = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(const Duration(seconds: 5), (Timer timer) {
+      // Find current page
+      int currentPage = controller.page!.toInt();
+      // Find next page, if in the last page, move to the initial page
+      int nextPage = currentPage == 3 ? 0 : currentPage + 1;
+      // Move to the next page
+      controller.animateToPage(
+        nextPage,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  // Closing App, cancel timer and dispose controller.
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: controller,
         children: [1, 2, 3, 4]
             .map(
               (e) => Image.asset(
