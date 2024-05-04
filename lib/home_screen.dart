eaa72aa0f0759c41672d49e1c4acc1659570022b
jeffKim <kwangho5538@gmail.com>
2024-05-04 11:38:05 +0900

    Add Images in PageView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: PageView(
+        children: [1, 2, 3, 4]
+            .map(
+              (e) => Image.asset(
+                'asset/img/page_$e.png',
+                fit: BoxFit.fill,
+              ),
+            )
+            .toList(),
+      ),
+    );
   }
 }
