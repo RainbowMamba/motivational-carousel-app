42553845b7dcb05ca174ea3ae6c6adaf4aac7b1b
jeffKim <kwangho5538@gmail.com>
2024-05-04 10:15:48 +0900

    First Commit

---
+++ android/app/src/main/kotlin/com/example/motivational_wallpapers/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.motivational_wallpapers
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:motivational_wallpapers/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
+
+
